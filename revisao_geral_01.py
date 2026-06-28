aad8065fbcd84838c782518d61caff64b70a2f54
cleiber-mata <cleiber.mata@gmail.com>
2026-06-28 00:48:20 -0300

    Desafio Geral 1 concluido: Unificacao dos blocos 1 ao 8

--- revisao_geral_01.py
+++ revisao_geral_01.py
@@ -1,10 +1,9 @@
 
 
-def calcular_faturamento():
-    mensalidade_bruta    = float(input("Digite o valor da mensalidade bruta: "))
-    quantidade_meses     = int(input("Digite a quantidade de meses: "))
+def calcular_faturamento(mensalidade_bruta, quantidade_meses):
+      
     faturamento_liquido = (mensalidade_bruta * quantidade_meses) * 0.95
-    manutencao_servidor = mensalidade_bruta * 0.05
+    manutencao_servidor = (mensalidade_bruta * quantidade_meses) * 0.05
     return faturamento_liquido, manutencao_servidor
 
 resposta = "S"
@@ -13,19 +12,21 @@ while resposta == "S":
     nome_usuario = input("Digite o nome do usuário: ").strip().title()
     email_usuario = input("Digite o e-mail do usuário: ").strip().lower()
     mensalidade = float(input("Digite o valor da mensalidade: "))
-    meses_ativos = int(input("Digite a quantidade de meses ativos: "))
+    meses_ativos = int(input("Digite a quantidade de meses ativos: ")) 
 
-    faturamento, manutencao = calcular_faturamento()
+    faturamento, manutencao = calcular_faturamento(mensalidade, meses_ativos)
 
     cliente_data = {
         "nome": nome_usuario,
         "email": email_usuario,
         "faturamento_liquido": faturamento
+        
     }
 
     print("\n========== RESUMO DO CLIENTE ==========")
     for chave, valor in cliente_data.items():
         print(f"{chave.capitalize()}: {valor}")
+
     if faturamento >= 500:
         print(f"nome: {cliente_data['nome']}: CLIENTE VIP - SUPORTE PREMIUM CORRESPONDENTE")
     else:
